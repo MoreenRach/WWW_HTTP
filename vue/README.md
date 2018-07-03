aa0f5fc6643892559f6c6637d3b4384cd6942614
Leonard Marschke <leonard@marschke.me>
2018-07-04 00:04:55 +0200

    initial import from html-training-resource

---
+++ vue/README.md
@@ -0,0 +1,30 @@
+# html-training-vue
+
+> Just another vue "education" project
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn install
+
+# serve with hot reload at localhost:8080
+yarn run dev
+
+# build for production with minification
+yarn run build
+
+# build for production and view the bundle analyzer report
+yarn run build --report
+
+# run unit tests -- we do not have unit tests in this project
+yarn run unit
+
+# run e2e tests -- we do not have end to end tests in this project
+yarn run e2e
+
+# run all tests -- we do not have tests at all :(
+yarn test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ vue/src/components/API.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <!--- Matriculation number(s): ??????, ?????? -->
+    <!--- It took me ? hours to solve the tasks in this file -->
+    <!--- It took me ? hours to set up the Vue application -->
+    <div class="container">
+      <h2>API "Content" - Page {{ currentPage }}</h2>
+
+      <!-- TODO second pagination like pagination below table here (1 Point) -->
+
+      <!-- Bonus points for a loading screen appearing when loading new entries -->
+      <b-card-group columns class="mb-4">
+        <b-card v-bind:key="entry['id']"
+                v-for="entry in pageEntries"
+                :header="entry['category']"
+                :img-src="getImageSrc(entry)"
+                img-fluid
+                img-alt="Image described by captions"
+                img-top>
+          <ul>
+            <!-- TODO insert list of captions here, consider caption['text'] as a unique value, use v-for to accomplish this (3 Points) -->
+          </ul>
+        </b-card>
+      </b-card-group>
+
+      <b-pagination align="center" size="md" :total-rows="totalCount" v-model="currentPage" :per-page="itemsPerPage"></b-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ITEMS_PER_PAGE } from '../constants'
+
+export default {
+  name: 'API',
+  data () {
+    return {
+      'currentPage': 1,
+      'itemsPerPage': ITEMS_PER_PAGE,
+      'pageEntries': [ // TODO remove this dummy image before hand in
+        {
+          'title': 'DUMMY',
+          'id': 1,
+          'category': 'No category',
+          'captions': [
+            {
+              'text': 'sometext'
+            }
+          ]
+        }
+      ],
+      'totalCount': 1
+    }
+  },
+  watch: {
+    currentPage: function () {
+      // trigger loading new items here (we do not care about race conditions here, but if you want to prevent them, you probably get some bonus points ;) )
+      this.loadActualPageEntries()
+    }
+  },
+  methods: {
+    loadActualPageEntries: function () {
+      console.log(this.currentPage)
+
+      // TODO load up to date page entries and store them in pageEntries (this.pageEntries) (3 Points)
+      // TODO Don't forget to update totalCount by request information! (1 Point)
+    },
+    getImageSrc: function (entry) {
+      // TODO get real address (1 Point)
+      return 'https://flask-training-api.www-technologien.marschke.me/v1/images/1/bitmap'
+    }
+  }
+  // TODO ensure loadActualPageEntries() gets called when the component gets displayed. Take a look at https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram (2 Points)
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../style/bootstrap-component-include';
+  .question {
+    color: $gray-700;
+  }
+
+  // TODO What does it do? Explain especially the @include command (1 Point class explanation, 2 Points include explanation)
+  .card-columns {
+    @include media-breakpoint-only(md) {
+      column-count: 2;
+    }
+    @include media-breakpoint-only(lg) {
+      column-count: 3;
+    }
+    @include media-breakpoint-only(xl) {
+      column-count: 4;
+    }
+  }
+</style>
